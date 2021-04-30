87c38d76e2f93c30aefc5dc8600a91909b286d22
Fadil <rosunmungurudm@gmail.com>
2021-05-01 01:17:07 +0400

    styling navigator

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ Debugger::enable(Debugger::DEVELOPMENT);
 Debugger::$dumpTheme = 'dark';
 
 const REPO = 'C:\wamp64\www\basic-bootstrap-theme';
+const APP_URL = 'http://localhost/basic-bootstrap-theme/';
 const URL = 'http://localhost/php-version-control/';
 
 # Use in windows OS
@@ -29,15 +30,31 @@ $branches = $repo->list_branches();
 <head>
 	<title>PHP version control</title>
 	<style>
-		select{
+		#git-version-control{
 			position: absolute;
-			background: #bbbbbb;
-			min-width: 100px;
+			background:transparent;
+			display: inline;
+			height: 40px;
+			min-width: 140px;
+			padding: 5px;
+			left: -145px;
+		}
+		select{
+			background: #FFF;
 			padding: 10px;
-			top: 5px;
-			left: 5px;
 			font-size: 15px;
+			width: 125px;
 			outline: none;
+			display: inline;
+			color: #000;
+		}
+		#toggle{
+			font-size: 31px;
+			text-align: center;
+			cursor: pointer;
+			display: inline;
+			vertical-align: middle;
+			color: #2196f3;
 		}
 	</style>
 	<script defer>
@@ -45,20 +62,31 @@ $branches = $repo->list_branches();
 			var url = element.options[element.selectedIndex].getAttribute('data-url');
 			window.location.href = url;
 		}
+
+		function toggleWrapper() {
+			var elem = document.getElementById('git-version-control');
+			elem.style.left = '0px';
+		}
 	</script>
 <head>
 <body style="margin: 0px">
-	<select onChange="changeBranch(this);">
-		<?php foreach ($branches as $branch): ?>
-			<option 
-				value="<?php echo $branch ?>" 
-				data-url="<?php echo URL ?>?branch=<?php echo $branch ?>&v=<?php echo rand(1, 99999999)?>"
-				<?php echo ($repo->active_branch() === $branch) ? "selected" : "" ?>
-			>
-				<?php echo $branch ?>
-			</option>
-		<?php endforeach; ?>
-	</select>
-	<iframe style="width:100%; height:100%; border:none;" src="http://localhost/basic-bootstrap-theme/"></iframe>
+	<div id="git-version-control">
+		<select onChange="changeBranch(this);">
+			<?php foreach ($branches as $branch): ?>
+				<option 
+					value="<?php echo $branch ?>" 
+					data-url="<?php echo URL ?>?branch=<?php echo $branch ?>"
+					<?php echo ($repo->active_branch() === $branch) ? "selected" : "" ?>
+				>
+					<?php echo $branch ?>
+				</option>
+			<?php endforeach; ?>
+		</select>
+		<div id="toggle" onclick="toggleWrapper()">&#8608;</div>
+	</div>
+	<iframe 
+		style="width:100%; height:100%; border:none;" 
+		src="<?php echo APP_URL; ?>" 
+		name="<?php echo 'cached-version-' . rand(1, 999999) ?>"></iframe>
 <body>
 </html>
